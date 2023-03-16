f7cb75b45f5eec88cdb7f79e546a6ba98c11b231
Robb-Fr <git@robb-i.net>
2023-03-16 14:22:24 +0000

    Adds specific filter to headers

--- lab0x01/src/filter.h
+++ lab0x01/src/filter.h
@@ -8,3 +8,4 @@ void filter_sepia(struct image *img, void *depth_arg);
 void filter_bw(struct image *img, void *threshold_arg);
 void filter_edge_detect(struct image *img, void *threshold_arg);
 void filter_keying(struct image *img, void *arg);
+void filter_specific_color(struct image *img, void *specific_color);
