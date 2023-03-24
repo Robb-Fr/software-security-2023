358fb1f3d69e09cba47b9ede870c2c3a57c74380
Robb-Fr <git@robb-i.net>
2023-03-24 18:51:04 +0100

    corrects setup script

--- lab0x02/setup.sh
+++ lab0x02/setup.sh
@@ -1,9 +1,14 @@
 #/bin/bash
 
 python3 -m venv ~/Desktop/venv && source ~/Desktop/venv/bin/activate
-echo "source ~/Desktop/venv/bin/activate" >> ~/.bashrc
+# echo "source ~/Desktop/venv/bin/activate" >> ~/.bashrc
 pip install pwntools
 
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
-setxkbmap ch
+chown -hR tdemont ./
+
+chmod +x ~/tsclient/SoftSecShared/software-security-2023/lab0x02/ghidra_10.2.3_PUBLIC/ghidraRun
+chmod +x ~/tsclient/SoftSecShared/software-security-2023/lab0x02/ghidra_10.2.3_PUBLIC/support/launch.sh
+
+setxkbmap ch fr_mac
