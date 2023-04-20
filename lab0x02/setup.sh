ce440cc308cf48144807b64019809e6a70d4a54b
Robb-Fr <git@robb-i.net>
2023-04-20 15:05:04 +0200

    lab2 dirty config and backup files

--- lab0x02/setup.sh
+++ lab0x02/setup.sh
@@ -6,7 +6,7 @@ pip install pwntools
 
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
-pip install r2env && r2env init && r2env add radare2@git
+# pip install r2env && r2env init && r2env add radare2@git
 
 chown -hR tdemont ./
 
